2e1ab5eae113967e56926eb980934106e9782ff6
Maicon Gretschmann <mr.gretschmann@gmail.com>
2025-06-09 00:30:51 -0300

    Contador de visitas

---
+++ script/script.js
@@ -0,0 +1,9 @@
+fetch('https://center-link-back.vercel.app/')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('contador').innerText = data.count;
+  })
+  .catch(error => {
+    console.error('Erro ao buscar contador:', error);
+    document.getElementById('contador').innerText = 'erro';
+  });

9079846ab388490c8cda06a04bdaf00412478a75
Maicon Gretschmann <mr.gretschmann@gmail.com>
2025-06-29 18:16:27 -0300

    Conter

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-fetch('https://center-link-back.vercel.app/')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('contador').innerText = data.count;
+fetch("https://center-link-back.vercel.app/api/contador")
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("contador").innerText = data.count;
   })
-  .catch(error => {
-    console.error('Erro ao buscar contador:', error);
-    document.getElementById('contador').innerText = 'erro';
+  .catch((error) => {
+    console.error("Erro ao buscar contador:", error);
+    document.getElementById("contador").innerText = "erro";
   });

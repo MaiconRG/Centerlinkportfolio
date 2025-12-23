9da8488ed8f52343626785b514521d0b3099a570
MaiconRG <mr.gretschmann@gmail.com>
2025-12-23 12:12:05 -0300

    Clear

--- script/script.js
+++
@@ -1,9 +0,0 @@
-fetch("https://center-link-back.vercel.app/api/contador")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("contador").innerText = data.count;
-  })
-  .catch((error) => {
-    console.error("Erro ao buscar contador:", error);
-    document.getElementById("contador").innerText = "erro";
-  });
